9eebdc55cff1e5f11c2e460f06a15aa2e58b8d09
Altered Art <cherokeedreemer@gmail.com>
2020-04-28 12:52:32 -0600

    pics

--- public/app.js
+++ public/app.js
@@ -1,22 +1,3 @@
-// const navSlide = () => {
-//   const burger = document.querySelector('.nav-menu');
-//   const nav = document.querySelector('.nav-links');
-//   const navLinks = document.querySelectorAll('.nav-links li');
-//
-//   burger.addEventListener('click', () => {
-//     nav.classList.toggle('nav-active');
-//     navLinks.forEach((link, index) => {
-//       if (link.style.animate) {
-//         link.style.animate = '';
-//       } else {
-//         link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
-//       }
-//     });
-//     burger.classList.toggle('toggle');
-//   });
-// }
-// navSlide();
-// notes
 const filterButtons=document.querySelector("#filter-btns").children;
 const items=document.querySelector(".portfolio-gallery").children;
 
@@ -39,8 +20,6 @@ const items=document.querySelector(".portfolio-gallery").children;
           items[k].style.display="block";
         }
       }
-        // console.log(this.getAttribute("data-target"))
-
     })
   }
 
@@ -104,5 +83,14 @@ const navbar = document.querySelector(".navbar");
           a[i].classList.remove("active");
         }
       this.classList.add("active")
+      document.querySelector(".navbar").classList.toggle("show");
     })
   })
+
+// ham burger menubar
+
+const menu = document.querySelector(".ham-burger");
+
+menu.addEventListener("click",function(){
+  document.querySelector(".navbar").classList.toggle("show");
+})
