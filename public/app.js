25049adac9de1eff876e630de85c3ceec786a48e
Altered Art <cherokeedreemer@gmail.com>
2020-10-04 20:31:09 -0600

    moving along with bugs

--- public/app.js
+++ public/app.js
@@ -1,89 +1,94 @@
-// filter buttons
-const filterButtons=document.querySelector("#filter-btns").children;
-const items=document.querySelector(".portfolio-gallery").children;
+// navbar links
+const navbar = document.querySelector(".navbar");
+a = navbar.querySelectorAll("a");
 
-
-  for(let i=0; i<filterButtons.length; i++){
-    filterButtons[i].addEventListener("click", function(){
-      for(let j=0; j<filterButtons.length; j++){
-         filterButtons[j].classList.remove("active")
-      }
-      this.classList.add("active");
-      const target=this.getAttribute("data-target")
-
-      for(let k=0; k<items.length; k++){
-        items[k].style.display="none";
-        if(target==items[k].getAttribute("data-id"))
-        {
-          items[k].style.display="block";
-        }
-        if(target=="all"){
-          items[k].style.display="block";
-        }
-      }
-    })
-  }
-// filter buttons
-// lightbox
-const closeLightbox=document.querySelector(".close-lightbox");
-const lightbox=document.querySelector(".lightbox");
-const lightboxImage=lightbox.querySelector("img");
-
-  lightbox.addEventListener("click", function(){
-    if(event.target!=lightboxImage){
-        lightbox.classList.remove("show");
-      lightbox.classList.add("hide");
+a.forEach(function (element) {
+  element.addEventListener("click", function () {
+    for (let i = 0; i < a.length; i++) {
+      a[i].classList.remove("active");
     }
-  })
-
-  closeLightbox.addEventListener("click", function(){
-    lightbox.classList.remove("show");
-    lightbox.classList.add("hide");
-})
-
-const gallery=document.querySelector(".portfolio-gallery");
-const galleryItem=gallery.querySelectorAll(".item");
-
-galleryItem.forEach(function(element){
-  element.querySelector(".fa-plus").addEventListener("click", function(){
-    lightbox.classList.remove("hide");
-    lightbox.classList.add("show");
-    lightboxImage.src=element.querySelector("img").getAttribute("src")
+    this.classList.add("active")
+    document.querySelector(".navbar").classList.toggle("show");
   })
 })
-// lightbox
+// nav links
+
 // header scroll
-window.onscroll=function(){
-  const docScrollTop=document.documentElement.scrollTop;
+window.onscroll = function () {
+  const docScrollTop = document.documentElement.scrollTop;
 
-  if(window.innerWidth>991){
-    if (docScrollTop>100){
-        document.querySelector("header").classList.add("fixed")
+  if (window.innerWidth > 991) {
+    if (docScrollTop > 100) {
+      document.querySelector("header").classList.add("fixed")
     }
-    else{
-    document.querySelector("header").classList.remove("fixed")
+    else {
+      document.querySelector("header").classList.remove("fixed")
     }
   }
 }
 // header scroll
-// navbar links
-const navbar = document.querySelector(".navbar");
-  a=navbar.querySelectorAll("a");
 
-  a.forEach(function(element){
-    element.addEventListener("click",function(){
-        for(let i=0; i<a.length; i++){
-          a[i].classList.remove("active");
-        }
-      this.classList.add("active")
-      document.querySelector(".navbar").classList.toggle("show");
-    })
-  })
-// nav links
 // ham burger menubar
 const menu = document.querySelector(".ham-burger");
 
-menu.addEventListener("click",function(){
+menu.addEventListener("click", function () {
   document.querySelector(".navbar").classList.toggle("show");
 })
 // ham burger menu
+
+// filter buttons
+const filterButtons = document.querySelector("#filter-btns").children;
+const items = document.querySelector(".portfolio-gallery").children;
+
+
+for (let i = 0; i < filterButtons.length; i++) {
+  filterButtons[i].addEventListener("click", function () {
+    for (let j = 0; j < filterButtons.length; j++) {
+      filterButtons[j].classList.remove("active")
+    }
+    this.classList.add("active");
+    const target = this.getAttribute("data-target")
+
+    for (let k = 0; k < items.length; k++) {
+      items[k].style.display = "none";
+      if (target == items[k].getAttribute("data-id")) {
+        items[k].style.display = "block";
+      }
+      if (target == "all") {
+        items[k].style.display = "block";
+      }
+    }
+  })
+}
+// filter buttons
+
+// lightBox
+const closeLightBox = document.querySelector(".close-lightBox");
+const lightBox = document.querySelector(".lightBox");
+const lightBoxImage = lightBox.querySelector("img");
+
+lightBox.addEventListener("click", function () {
+  if (event.target != lightBoxImage) {
+    lightBox.classList.remove("show");
+    lightBox.classList.add("hide");
+  }
+})
+
+closeLightBox.addEventListener("click", function () {
+  lightBox.classList.remove("show");
+  lightBox.classList.add("hide");
+})
+
+const gallery = document.querySelector(".portfolio-gallery");
+const galleryItem = gallery.querySelectorAll(".item");
+
+galleryItem.forEach(function (element) {
+  element.querySelector(".fa-plus").addEventListener("click", function () {
+    lightBox.classList.remove("hide");
+    lightBox.classList.add("show");
+    lightBoxImage.src = element.querySelector("img").getAttribute("src")
+  })
+})
+// lightBox
+
+
